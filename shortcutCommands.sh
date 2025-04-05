914f1a7efa025cdb5c9bf3e945e26144ccf36e6a
Samsontanimawo <engr.tanimawo@gmail.com>
2025-04-05 13:49:55 -0500

    Added new functionalites.

--- shortcutCommands.sh
+++ shortcutCommands.sh
@@ -6,3 +6,5 @@
 6. ./serve-frontend.sh
 
 find . -type f -name "*.sh" -exec chmod +x "{}" \; = Find and make all .sh files executable
+
+Migrated Nova CDN project to AWS. Project live on http://13.217.223.190:8081
