9c03a897e8697155ed4cb82af6f6cf3b925a7927
Samsontanimawo <engr.tanimawo@gmail.com>
2025-04-05 09:17:21 -0500

    Added new functionalites.

--- serve-frontend.sh
+++ serve-frontend.sh
@@ -6,8 +6,12 @@ cd novacdn-frontend/ || { echo "❌ Directory not found!"; exit 1; }
 echo "Building the React frontend..."
 npm run build
 
-echo "Serving the build with 'serve -s build'..."
-serve -s build
+echo "Installing 'serve' globally if not already installed..."
+npm install -g serve
+
+echo "Serving the build with 'serve -s build -l 3000'..."
+serve -s build -l 3000
+
 
 # chmod +x serve-frontend.sh
 # ./serve-frontend.sh
